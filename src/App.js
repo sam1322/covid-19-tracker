2b53808ea11b1c20f59d15788571a3c71f2eb3c3
sam1322 <sriram.pant5@gmail.com>
2022-03-11 22:43:08 +0530

    Made some more changes in the header of the webpage using Material UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import './App.css';
+import React , {useState,useEffect} from 'react'
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries,setCountries] = useState(['USA','UK','India'])
   return (
-    <div className="App">
-      <h1>
-      COVID-19 TRACKER 
-      </h1>
-      
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            
+            {
+              countries.map((country,i)=>(<MenuItem key={i} value={country}>{country}</MenuItem>))
+            }
+            
+            {/* <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="Yooo">Yooo</MenuItem>
+            <MenuItem value="Hey2y">Hey2y</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+
       {/* Header */}
       {/* Title + Select input dropdown field */}
       {/* Infobocs */}
@@ -17,7 +34,6 @@ function App() {
       {/* Graph */}
 
       {/* Map */}
-
     </div>
   );
 }
